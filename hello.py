fc02ca46804603516422956aeca938990606aa67
Mounire <zy.freesnow@hotmail.com>
2017-08-20 20:12:59 +0800

    add template files

---
+++ hello.py
@@ -0,0 +1,42 @@
+from flask import Flask
+from flask import render_template, session, redirect, url_for, flash
+from flask_bootstrap import Bootstrap
+from forms import NameForm
+
+app = Flask(__name__)
+app.config['SECRET_KEY'] = 'key'
+bootstrap = Bootstrap(app)
+
+
+@app.route('/', methods=['GET', 'POST'])
+def index():
+    form = NameForm()
+    if form.validate_on_submit():
+        old_name = session.get('name')
+        if old_name is not None and old_name != form.name.data:
+            flash('Looks like you have changed your name!')
+        session['name'] = form.name.data
+        return redirect(url_for('index'))
+    return render_template('index.html', form=form, name=session.get('name'))
+
+
+@app.route('/users/<name>/')
+def user(name=None):
+    if name is None:
+        return render_template('index.html')
+    else:
+        return render_template('user.html', name=name)
+
+
+@app.errorhandler(404)
+def page_not_found(e):
+    return render_template('404.html'), 404
+
+
+@app.errorhandler(500)
+def internal_server_error(e):
+    return render_template('500.html'), 500
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
